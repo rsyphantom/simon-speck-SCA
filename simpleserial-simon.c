16210067805630e6275faa1db6362594e11d932e
rsyphantom <147994520+rsyphantom@users.noreply.github.com>
2026-03-04 22:38:08 +0800

    Update simpleserial-simon.c

--- simpleserial-simon.c
+++ simpleserial-simon.c
@@ -15,7 +15,7 @@ uint16_t ct[2] = {0x00};
 // 轮密钥数组
 uint16_t round_keys[ROUNDS];
 
-// Simon 的常量 z 序列
+// Simon 的常量 
 const uint64_t z0 = 0b11111010001001010110000111001101111101000100101011000011100110;
 
 // 单轮加密函数
@@ -38,57 +38,16 @@ void key_schedule(uint16_t *key) {
     
     for (int i = 4; i < ROUNDS; i++) {
         uint16_t tmp = ror(round_keys[i-1], 3);
-        // Simon 32/64 m=4 的密钥扩展核心逻辑
+
         tmp ^= round_keys[i-3];
         tmp ^= ror(tmp, 1);
         
         uint8_t z_bit = (z0 >> ((i - 4) % 62)) & 1;
         
-        // 注意：去掉 ~ 号，直接使用 c
         round_keys[i] = round_keys[i-4] ^ tmp ^ z_bit ^ c;
     }
 }
 
-// 加密主函数
-// void encrypt(uint16_t *pt, uint16_t *ct, uint16_t *key) {
-//     // 生成所有轮密钥
-//     key_schedule(key);
-    
-//     uint16_t left = pt[1];
-//     uint16_t right = pt[0];
-    
-    
-//     // 执行 32 轮加密
-//     for (int i = 0; i < ROUNDS; i++) {
-//         trigger_high();
-//         enc_one_round(&left, &right, round_keys[i]);
-//         trigger_low();
-//     }
-    
-//     ct[0] = left;
-//     ct[1] = right;
-// }
-
-// void encrypt(uint16_t *pt, uint16_t *ct, uint16_t *key) {
-//     // 生成所有轮密钥
-//     key_schedule(key);
-    
-//     uint16_t left = pt[1];
-//     uint16_t right = pt[0];
-    
-//     trigger_high(); // <<<--- 在加密开始时拉高
-//     // 执行 32 轮加密
-//     for (int i = 0; i < ROUNDS; i++) {
-//         enc_one_round(&left, &right, round_keys[i]);
-//     }
-//     trigger_low();  // <<<--- 在加密结束时拉低
-    
-//     ct[0] = left;
-//     ct[1] = right;
-// }
-
-
-// 假设您将 enc_one_round 恢复到不带触发的版本，并将触发逻辑移入 encrypt
 void encrypt(uint16_t *pt, uint16_t *ct, uint16_t *key) {
     key_schedule(key);
     uint16_t left = pt[1];
@@ -106,7 +65,6 @@ void encrypt(uint16_t *pt, uint16_t *ct, uint16_t *key) {
             trigger_low();  // 仅在第一轮结束时拉低
         }
     }
-    // ...
 }
 uint8_t get_key(uint8_t* k, uint8_t len)
 {
